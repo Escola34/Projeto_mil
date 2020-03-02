5b48193a511ffde55ba964e388c7378f857e733a
Raphael Gibson <42935757+raphaelgibson@users.noreply.github.com>
2020-03-02 14:20:59 -0300

    Update Linear.py
    Ajustes na estilização do código.

--- Linear.py
+++ Linear.py
@@ -12,32 +12,32 @@ from iqoptionapi.stable_api import IQ_Option
 
 
 def buy():
-	#Função de compra
-	I_want_money=IQ_Option("","")
-	Money=10
-	ACTIVES="EURUSD"#Ativo para compra
-	ACTION="call"
-	expirations_mode=1
-	id=I_want_money.buy(Money,ACTIVES,ACTION,expirations_mode)
-	id2=I_want_money.buy(Money,ACTIVES,ACTION,expirations_mode)
+	# Função de compra
+	i_want_money     = IQ_Option("","")
+	money            = 10
+	actives          = "EURUSD" # Ativo para compra
+	action           = "call"
+	expirations_mode = 1
+	id               = i_want_money.buy(money, actives, action, expirations_mode)
+	id2              = i_want_money.buy(money, actives, action, expirations_mode)
 
 
-while  True:
+while True:
 	try:
-		time.sleep(59)#Resultado para cada 59 segundos. Altere se quiser
-		dataframe = pd.DataFrame()
-		read = pd.read_csv("iqdataset.csv", delimiter=";")
+		time.sleep(59) # Resultado para cada 59 segundos. Altere se quiser
+		dataframe      = pd.DataFrame()
+		read           = pd.read_csv("iqdataset.csv", delimiter = ";")
 		dataframe['x'] = read["timestamp"] 
 		dataframe['y'] = read["varkandle"] 
-		x_values = np.log(dataframe[['x']])
-		y_values = np.log(dataframe[['y']])
-		model = linear_model.LinearRegression()
-		fit= model.fit(x_values, y_values)
-		preditx = model.predict(x_values)
-		MSE = mean_squared_error(y_values, preditx)
-		CED = r2_score(y_values, preditx)
+		x_values       = np.log(dataframe[['x']])
+		y_values       = np.log(dataframe[['y']])
+		model          = linear_model.LinearRegression()
+		fit            = model.fit(x_values, y_values)
+		preditx        = model.predict(x_values)
+		MSE            = mean_squared_error(y_values, preditx)
+		CED            = r2_score(y_values, preditx)
+		tu             = strftime("%H:%M:%S")
 		print("MeSE", MSE)
-		tu = strftime("%H:%M:%S")
 		print("R2:%.2f" %(CED), "|", tu)
 	except  Exception as E:
 		print(E)
