9a92ea0779a88ece7d4e6ff8031c78c2fcd62bf2
Raphael Gibson <42935757+raphaelgibson@users.noreply.github.com>
2020-03-02 14:26:59 -0300

    Update Yahoo.py
    Ajustes de estilização do código, substituição de format por f-string, uso da função de arredondamento round no lugar de %.2f.

--- Yahoo.py
+++ Yahoo.py
@@ -1,27 +1,26 @@
 #! /usr/bin/python3
 from yahoo_finance_api2 import share
 import pprint, time
-from datetime import  datetime
+from datetime import datetime
 
 while True:
     try:
         teste = share.Share("BTC-USD")
-        date = None
-        date = teste.get_historical(share.PERIOD_TYPE_DAY, 1, share.FREQUENCY_TYPE_MINUTE, 5)
-        pp = pprint.PrettyPrinter(depth=7)
+        date  = None
+        date  = teste.get_historical(share.PERIOD_TYPE_DAY, 1, share.FREQUENCY_TYPE_MINUTE, 5)
+        pp    = pprint.PrettyPrinter(depth = 7)
         
-        ope = date['open'][-1]
-        high = date['high'][-1]
-        low = date['low'][-1]
+        ope   = date['open'][-1]
+        high  = date['high'][-1]
+        low   = round(date['low'][-1], 2)
         close = date['close'][-1]
-        low = "%.2f" %(low)          
 
-        now =datetime.now()
-        hora = now.strftime('%d-%m-%y;%H:%M:%S')
-        al = str('{};{}'.format(hora, low))
+        now   = datetime.now()
+        hora  = now.strftime('%d-%m-%y;%H:%M:%S')
+        al    = str(f'{hora};{low}\n')
         print(al)
         with open('dataset_Yahoo.csv', 'a') as csvfile:
-            file = csvfile.write(al+'\n')
+            file = csvfile.write(al)
 
         time.sleep(60)
     except Exception as e:
